a922c0a658238b439e4d2127acf6b3415b494e93
ramses <ramses69@naver.com>
2024-05-16 16:26:37 +0900

    설정파일

--- fishing/src/main/java/com/stayc/FishingApplication.java
+++ fishing/src/main/java/com/stayc/FishingApplication.java
@@ -2,8 +2,9 @@ package com.stayc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FishingApplication {
 
 	public static void main(String[] args) {
