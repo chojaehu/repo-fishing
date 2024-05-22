b92d2cea43c66ebc142041e7aa1ae094fabcc14d
ramses <ramses69@naver.com>
2024-05-22 16:20:20 +0900

    초기설정

---
+++ fishing/src/main/java/com/stayc/common/constants/Constants.java
@@ -0,0 +1,29 @@
+package com.stayc.common.constants;
+
+public class Constants {
+	public static final String PATH_CODE 		= "infra/v1/code/";
+	public static final String PATH_CODE_GROUP 	= "infra/v1/codegroup/";
+	public static final String PATH_HOME 		= "infra/v1/home/";
+	public static final String PATH_LOGIN 		= "infra/v1/login/";
+	public static final String PATH_MEMBER 		= "infra/v1/member/";
+	public static final String PATH_PRODUCT 	= "infra/v1/product/";
+	public static final String PATH_TEMP 		= "infra/v1/temp/";
+	
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	
+	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
+	
+	public static final int DATE_INTERVAL = -10;
+	
+	public static final int SESSION_MINUTE_SDM = 30;
+	
+	public static final String UPLOADED_PATH_PREFIX_LOCAL          = "D:\\factory\\workspace\\nas/"; // 저장위치
+	public static final String UPLOADED_PATH_PREFIX_FOR_VIEW_LOCAL = "/images/";                  // 보여줄위치
+	
+	public static final String UPLOADED_PATH_PREFIX_LOCAL_MAC          = "/Volumes/Work/factory/workspace/nas/"; // 저장위치
+	public static final String UPLOADED_PATH_PREFIX_FOR_VIEW_LOCAL_MAC = "/images/";                  // 보여줄위치
+	
+	public static final String UPLOADED_RESSOURCE_HANDLER = "/images/**";
+	public static final String UPLOADED_RESSOURCE_LOCATIONS = "file:///Volumes/Work/factory/workspace/nas/";
+	
+}
