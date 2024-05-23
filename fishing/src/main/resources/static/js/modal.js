ea9c11704014a485bbc0970a4da833a02c251826
ramses <ramses69@naver.com>
2024-05-23 23:32:03 +0900

    메뉴수정, 로그인 작업중

--- fishing/src/main/resources/static/js/modal.js
+++ fishing/src/main/resources/static/js/modal.js
@@ -1,8 +1,6 @@
-
-// 사용자
 // 모달폼 띄우기
 fnModalShow = function(paramTitle, paramContent) {
 	$("#title").html(paramTitle);
-	$("#modalFormOneBtnContent").html(paramContent);
-	$("#modalFormOneBtn").modal('show');
+	$("#modalConfirmContent").html(paramContent);
+	$("#modalConfirm").modal('show');
 }
