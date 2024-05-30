78a8cfc30a1eb72b5fe5ff1aeff15cdb7510c51e
chojaehu <fhfh5741@gmail.com>
2024-05-30 14:23:04 +0900

    파이어 베이스 config수정

--- fishing/src/main/java/com/stayc/common/config/FireBaseConfig.java
+++ fishing/src/main/java/com/stayc/common/config/FireBaseConfig.java
@@ -17,7 +17,7 @@ public class FireBaseConfig {
 	@PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/serviceAccountKey.json");
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/fishing-9e7dc-firebase-adminsdk-jc98j-b5455f934e.json");
 
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
