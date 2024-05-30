09279e7ab88240b91d756afb665d3482747006d3
doccomsa <dlaudfh01@gmail.com>
2024-05-30 17:50:18 +0900

    예약수정

--- fishing/src/main/java/com/stayc/infra/reservation/reservationController.java
+++ fishing/src/main/java/com/stayc/infra/reservation/reservationController.java
@@ -1,5 +1,7 @@
 package com.stayc.infra.reservation;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,11 +28,31 @@ public class reservationController {
 		
 		return "infra/reserve/seat";
 	}
-	
+//	결제페이지
 	@RequestMapping(value = "/checkout")
 	public String checkout(reservationDto dto, Model model) throws Exception {
 		
+		System.out.println(dto.getPlcSeq() + "---------------1");
+		System.out.println(dto.getPlcName() + "---------------2");
+		System.out.println(dto.getPlcTypeCd() + "--------------3");
+		System.out.println(dto.getPlcAmount() + "---------------4");
+		System.out.println(dto.getRevSeat() + "------------------5");
+		System.out.println(dto.getRevDate() + "-------------------6");
 		
+		String plcSeq = dto.getPlcSeq();
+		String plcName = dto.getPlcName();
+		String plcTypeCd = dto.getPlcTypeCd();
+	    Double plcAmount = dto.getPlcAmount();
+	    int revSeat = dto.getRevSeat();
+	    Date revDate = dto.getRevDate();
+		
+	    model.addAttribute("plcSeq", plcSeq);
+	    model.addAttribute("plcName", plcName);
+	    model.addAttribute("plcTypeCd", plcTypeCd);
+	    model.addAttribute("plcAmount", plcAmount);
+	    model.addAttribute("revSeat", revSeat);
+	    model.addAttribute("revDate", revDate);
+	    
 		return "infra/reserve/checkout";
 	}
 }
