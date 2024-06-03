8aed87089ed08bc8107026a315e53344ed254ffe
chojaehu <fhfh5741@gmail.com>
2024-06-03 15:49:15 +0900

    채팅구현

--- fishing/src/main/java/com/stayc/infra/chattalk/ChattalkService.java
+++ fishing/src/main/java/com/stayc/infra/chattalk/ChattalkService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 
 @Service
 public class ChattalkService {
@@ -27,12 +28,15 @@ public class ChattalkService {
 //	파이어 베이스 인설트 테스트
 	public void chattinginst(ChattalkDto dto ) throws Exception {
 		String roomSeq = String.valueOf(dto.getRomSeq());
-		//ApiFuture<WriteResult> apiFuture = firestore.collection("fishing").document("user_3").set(dto);
-		ApiFuture<DocumentReference> apiFuture = firestore.collection(roomSeq).document(dto.getMbrSeq()).collection("messages").add(dto);
+		String rtkSeq = String.valueOf(dto.getRtkSeq());
+		ApiFuture<WriteResult> apiFuture = firestore.collection(roomSeq).document(rtkSeq).set(dto);
 
 	    // ApiFuture 결과 처리
-	    DocumentReference documentReference = apiFuture.get(); // 블로킹 호출
-	    System.out.println("Document added with ID: " + documentReference.getId());
+		 WriteResult writeResult = apiFuture.get(); // 블로킹 호출
+
+        
+        //DocumentReference documentReference = firestore.collection(roomSeq).document(rtkSeq);
+        //System.out.println("Document added with ID: " + documentReference.getId());
 
     }
 
