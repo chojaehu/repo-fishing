fa704d00b1325d63791db1220c5d7c1e2d7ce500
chojaehu <fhfh5741@gmail.com>
2024-06-03 16:11:00 +0900

    수정

--- fishing/src/main/java/com/stayc/infra/chattalk/ChattalkService.java
+++ fishing/src/main/java/com/stayc/infra/chattalk/ChattalkService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 
